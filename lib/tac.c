629123574d87aec93ca9d64f18203a71ae1d4748
Robert Schumacher <ras0219@outlook.com>
2022-12-16 19:09:20 -0800

    Simplify debug_taca

--- lib/tac.c
+++ lib/tac.c
@@ -35,40 +35,42 @@ const char* register_to_string(enum Register k)
 
 void debug_tace(Array* arr, const TACEntry* tace)
 {
-    array_push_byte(arr, '{');
+    array_push_byte(arr, '(');
     array_appends(arr, taco_to_string(tace->op));
-    array_push_byte(arr, ',');
     array_push_byte(arr, ' ');
     debug_taca(arr, &tace->arg1);
-    array_push_byte(arr, ',');
     array_push_byte(arr, ' ');
     debug_taca(arr, &tace->arg2);
-    array_push_byte(arr, '}');
+    array_push_byte(arr, ')');
 }
+
+void debug_sizing(Array* arr, Sizing sz) { array_appendf(arr, "%c%d", sz.is_signed ? 'i' : 'u', sz.width); }
 
 void debug_taca(Array* arr, const TACAddress* addr)
 {
-    array_push_byte(arr, '{');
+    array_push_byte(arr, '(');
     array_appends(arr, taca_to_string(addr->kind));
-    if (addr->is_addr) array_appends(arr, ", .is_addr = 1");
+    if (addr->is_addr) array_appends(arr, " is_addr");
     if (addr->sizing.width != 0)
     {
-        array_appendf(arr, ", .sizing = %c, %d", addr->sizing.is_signed ? '1' : '0', addr->sizing.width);
+        array_push_byte(arr, ' ');
+        debug_sizing(arr, addr->sizing);
     }
     switch (addr->kind)
     {
-        case TACA_FRAME: array_appendf(arr, ", .frame_offset = %d}", addr->frame_offset); break;
-        case TACA_IMM: array_appendf(arr, ", .imm = %zu}", addr->imm); break;
-        case TACA_ARG: array_appendf(arr, ", .arg_offset = %zu}", addr->arg_offset); break;
-        case TACA_REG: array_appendf(arr, ", .reg = %s}", register_to_string(addr->reg)); break;
-        case TACA_REF: array_appendf(arr, ", .ref = %zu}", addr->ref); break;
-        case TACA_ALABEL: array_appendf(arr, ", .alabel = %zu}", addr->alabel); break;
-        case TACA_LLABEL: array_appendf(arr, ", .literal = %s}", addr->literal); break;
-        case TACA_PARAM: array_appendf(arr, ", .param_offset = %zu}", addr->param_offset); break;
-        case TACA_NAME: array_appendf(arr, ", .name = \"%s\"}", addr->name ? addr->name : "(null)"); break;
-        case TACA_LNAME: array_appendf(arr, ", .name = \"%s\"}", addr->name ? addr->name : "(null)"); break;
-        case TACA_CONST: array_appendf(arr, ", .const_idx = %zu}", addr->const_idx); break;
-        case TACA_VOID: array_push_byte(arr, '}'); break;
-        default: array_appendf(arr, ", unimplemented = %p}", addr); break;
+        case TACA_FRAME: array_appendf(arr, " %d", addr->frame_offset); break;
+        case TACA_IMM: array_appendf(arr, " %zu", addr->imm); break;
+        case TACA_ARG: array_appendf(arr, " %zu", addr->arg_offset); break;
+        case TACA_REG: array_appendf(arr, " %s", register_to_string(addr->reg)); break;
+        case TACA_REF: array_appendf(arr, " %zu", addr->ref); break;
+        case TACA_ALABEL: array_appendf(arr, " %zu", addr->alabel); break;
+        case TACA_LLABEL: array_appendf(arr, " %s", addr->literal); break;
+        case TACA_PARAM: array_appendf(arr, " %zu", addr->param_offset); break;
+        case TACA_NAME: array_appendf(arr, " \"%s\"", addr->name ? addr->name : "(null)"); break;
+        case TACA_LNAME: array_appendf(arr, " \"%s\"", addr->name ? addr->name : "(null)"); break;
+        case TACA_CONST: array_appendf(arr, " %zu", addr->const_idx); break;
+        case TACA_VOID: break;
+        default: array_appendf(arr, " unimplemented", addr); break;
     }
+    array_push_byte(arr, ')');
 }
